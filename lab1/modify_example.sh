05ade27db4181a49832058f0995c47c81705447a
swierszczyyk <37833417+Swierszcz@users.noreply.github.com>
2021-03-23 11:01:24 +0100

    Update modify_example.sh

--- lab1/modify_example.sh
+++ lab1/modify_example.sh
@@ -2,9 +2,11 @@
 mkdir dir1
 cd dir1
 touch test.c
+touch upper
+touch text
 mkdir dir2
 cd dir2
-touch pathtest.c
+touch pathtest
 cd ..
 
 ###Creating tree of files for recursive tests
@@ -53,11 +55,19 @@ ls dir1
 echo "After: " 
 ls dir1
 echo "
-Sed pattern test in dir2, replace t with g: ./modify.sh 's/test/success/g' dir1/dir2/pathtest.c
+Uppercasing few files in dir1: ./modify.sh -u dir1/test.c dir1/upper dir1/text 
+dir1:"
+echo "Before: " 
+ls dir1
+./modify.sh -u dir1/test.c dir1/upper dir1/text        
+echo "After: " 
+ls dir1
+echo "
+Sed pattern test in dir2, replace test with _success: ./modify.sh 's/test/_success/g' dir1/dir2/pathtest.c
 dir1:"
 echo "Before: " 
 ls dir1/dir2
-./modify.sh 's/c/d/g' dir1/dir2/pathtest.c
+./modify.sh 's/test/_success/g' dir1/dir2/pathtest
 echo "After: "
 ls dir1/dir2
 
@@ -80,8 +90,10 @@ ls dir1/rec_dir2/dir11/dir12
 echo "
 dir13: "
  ls dir1/rec_dir2/dir11/dir13
+ echo "
+Use of recursive mode + sed, f changed for Frag: ./modify.sh -r 's/f/Frag/g' dir1/rec_dir2"
 ./modify.sh -r 's/f/Frag/g' dir1/rec_dir2
-echo "-----
+echo "
 After: "
 echo "
 rec_dir2: "
@@ -104,45 +116,32 @@ TESTING: SIMPLE, IMPROPER USAGE:"
 echo "
 Uppercasing non-existing file in dir1: ./modify.sh -u dir1/blabla.c
 dir1:"
-echo "Before: " 
-ls dir1
 ./modify.sh -u dir1/blabla.c          
-echo "After: " 
-ls dir1
 echo "
 Uppercasing existing file in non-existing dir0: ./modify.sh -u dir0/test.c
 dir1:"
-echo "Before: " 
-ls dir1
 ./modify.sh -u dir0/test.c          
-echo "After: " 
-ls dir1
 echo "
 Wrong input into dir1: ./modify.sh 'blablabla' dir1/test.c    
 dir1:"
-echo "Before: " 
-ls dir1
 ./modify.sh blablabla dir1/test.c         
-echo "After: " 
-ls dir1
 echo "
 Wrong input into dir1: ./modify.sh 's/bla/failure/g' dir1/test.c    
 dir1:"
-echo "Before: " 
-ls dir1
 ./modify.sh 's/bla/failure/g' dir1/test.c         
-echo "After: " 
-ls dir1
 echo "
 No files given: ./modify.sh -l dir1
 dir1:"
-echo "Before: " 
-ls dir1
 ./modify.sh -l dir1         
-ls dir1
 echo "
 No directory/files given: ./modify.sh -l"
-./modify.sh -l              
+./modify.sh -l    
+echo "
+Two arguments given: ./modify.sh -l -u dir1/test.c"
+./modify.sh -l -u dir1/test.c      
+echo "
+Wrong argument given given: ./modify.sh -lu dir1/test.c"
+./modify.sh -lu dir1/test.c       
 echo "=================================================
 TESTING: RECURSIVE, IMPROPER USAGE:"
 ./modify.sh -r 's/Frag/f/g' dir1/rec_dir2
@@ -154,5 +153,5 @@ No directory given: ./modify.sh -r -u"
 ./modify.sh -r -u
 echo "
 Given file instead of directory: ./modify.sh -r -u dir1/rec_dir2/file2"
-./modify.sh -r -u dir1/rec_dir2/file2    
+./modify.sh -r -u dir1/rec_dir2/file2               
 rm -r dir1
