6f53078dee58b9972bbd704f2015909b4399213e
swierszczyyk <37833417+Swierszcz@users.noreply.github.com>
2021-04-10 19:10:51 +0200

    further changes
    changes as specified in the lab script

--- lab2/tsig.c
+++ lab2/tsig.c
@@ -5,37 +5,60 @@
 #include <sys/wait.h>
 
 #define NUM_CHILD 7
+#define INFINITE for(;;)
+#define WITH_SIGNALS
+
+#ifdef WITH_SIGNALS
+    int key_pressed = 0;
+#endif 
+
+/* interrupt signal: default =  ctrl+c */
 
 void child_proccess(){
     /* Child Process algorithm */
 
-    printf("Child[%d] of parent[%d]\n", getpid(), getppid());
+    printf("Child[%d]: under Parent[%d]\n", getpid(), getppid());
     sleep(10);
-    printf("Execution completed for child[%d]\n", getpid());
+    printf("Child[%d]: Execution completed\n", getpid());
 }
 
 void child_killer(int i, pid_t pids[]){
     /* Child processes killing function */
-
-    printf("Fork failed\n");
-
     for (int j = 0; j < i; j++){
     
-        printf("Child[%d] killed\n", pids[j]);
         kill(pids[j], SIGTERM);
     
     }
 }
+
+#ifdef WITH_SIGNALS
+void sig_int_pressed(int signal){
+
+    //printf("Interrupt triggered for Parent[%d]\n", getpid());
+    key_pressed = 1;
+
+}
+#endif 
 
 int main(void)
 {
     int i;
     pid_t pid;
     pid_t children_pids[NUM_CHILD];
-    int child_num = 0;  // in case that child_killer is used
     printf("\nINITIAL PARENT PID: %d\n\n", getpid());
+    
+    /* Ignoring all signals with the signal() */
+    
+    #ifdef WITH_SIGNALS
+        for(int it = 0; it < _NSIG; it++){
+            signal(it, SIG_IGN);
+        }
 
-    /*Main process algorithm*/
+        signal(SIGCHLD, SIG_DFL);
+        signal(SIGINT, sig_int_pressed);
+    #endif
+    
+    /* Main process algorithm */
     for(i = 0; i < NUM_CHILD; i++){
         
         switch(pid = fork()){
@@ -44,34 +67,51 @@ int main(void)
                 child_proccess();
                 return 0;
             case -1:
+                printf("Fork failed\n");
                 child_killer(i, children_pids);
                 return 1;
             default:
                 children_pids[i] = pid;
-                printf("Parent[%d] with child[%d]\n", getpid(), children_pids[i]);
+                printf("Parent[%d]: with new Child[%d]\n", getpid(), children_pids[i]);
                 break;
         }
-
-        child_num++;
+        #ifdef WITH_SIGNALS
+            if(key_pressed == 1){
+                printf("Parent[%d]: Creation process interrupted\n", getpid());
+                child_killer(i, children_pids);
+            }
+        #endif
         sleep(1);
     }
 
     int stat, num_zero = 0, num_one = 0;
+    int j = 0;
 
-    for(int j = 0; j < child_num; j++){
+    INFINITE{
 
         pid = wait(&stat);
-        printf("%d: Exit status of child[%d]: %d\n", j+1, children_pids[j], WEXITSTATUS(stat));
+        if(pid == -1)
+            //printf("Stopping loop\n");
+            break;
+
+        printf("Child[%d]: Exit status: %d\n", children_pids[j], WEXITSTATUS(stat));
+        
         if(WEXITSTATUS(stat) == 0)
             num_zero++;
         else
             num_one++;
-    
+        j++;
     }
 
-    printf("\nNo more children for parent[%d].\n", getpid());
+    printf("\nNo more children for Parent[%d].\n", getpid());
     printf("Number of recieved 0 exit codes: %d\n", num_zero);
     printf("Number of recieved 1 exit codes: %d\n\n", num_one);
+    
+    #ifdef WITH_SIGNALS
+        for(int it = 0; it < _NSIG; it++){
+            signal(it, SIG_DFL);
+        }
+    #endif
     
     return 0;
 }
